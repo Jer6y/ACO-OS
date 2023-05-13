8d84ff115f4868080b77c3a1f7dc4bed24cc2618
Jerry_Luo <3229960773@qq.com>
2023-05-13 14:35:02 +0800

    将与csr有关的位的意义移到了riscv.h

--- src/include/platform.h
+++ src/include/platform.h
@@ -5,7 +5,7 @@
 #define CPUS 1
 
 //内核基地址 闭区间
-#define KERNEL_BASE (0x80000000ul)
+#define KERNEL_BASE (0x80000000ull)
  
 //页粒度 4k
 #define PAGESIZE (4096)
@@ -13,6 +13,5 @@
 //物理地址结束的地方 闭区间
 #define RAM_END  ((KERNEL_BASE+128*1024*1024)-1)
 
-#define SIE (1<<1)
 
 #endif
