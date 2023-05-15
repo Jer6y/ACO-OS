82e916fac7f7e0adf5b10c2c7febc43881675a42
Jerry_Luo <3229960773@qq.com>
2023-05-15 17:01:11 +0800

    增加同步输出

--- src/kernel/uart.c
+++ src/kernel/uart.c
@@ -1,3 +1,31 @@
 #include <def.h>
+#include <uart.h>
+#define BUF_SIZE 128
+// static output_buffer[BUF_SIZE];
+// static uint32 w;
+// static uint32 r;
 
-static output_buffer;
+void uart_init()
+{
+    //关闭中断
+    write_reg(IER,0,uint8);
+    
+    //设置波特率
+    write_reg(LCR,1<<7,uint8);
+    write_reg(BUAD_LOW,0x3,uint8);
+    write_reg(BUAD_HIGH,0x0,uint8);
+    
+    //设置字模式和校验
+    write_reg(LCR,LCR_WORD_LENGTH,uint8);
+
+    //打开FIFO并清空
+    write_reg(FCR,FCR_FIFO_ENABLE|FCR_RX_FIFO_CLEAR|FCR_TX_FIFO_CLEAR,uint8);  
+
+    write_reg(IER,IER_RX_INTA|IER_TX_INTA,uint8);
+}
+void uart_putc_sync(char c)
+{
+    uint8 state =0;
+    while(!((state=read_reg(LSR,uint8))&LSR_TX_READY)) ;
+    write_reg(THR,c,uint8);
+}
