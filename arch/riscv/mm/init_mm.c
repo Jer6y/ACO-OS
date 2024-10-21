40ddf0e04b79c30541474f55e444138f3a81e54d
Jer6y <3229960773@qq.com>
2024-10-21 20:53:35 +0800

    page : rename PAGE_ALIGN to PAGE_ALIGN_BIT

--- arch/riscv/mm/init_mm.c
+++ arch/riscv/mm/init_mm.c
@@ -1,12 +1,18 @@
 #include <aco/types.h>
 #include <aco/linkage.h>
+#include <mm/page.h>
 #include <mm/memblock_alloc.h>
-#include <aco/init.h>
+#include <aco/printf.h>
+
+PRIVATE_VAR mmblock_hdr_t early_mmallocator;
 
 void setup_mm()
 {
 	uintptr_t heap_start = (uintptr_t)get_section_start(initheap);
 	uintptr_t heap_end = (uintptr_t)get_section_end(initheap);
+	//ASSERT(heap_end - heap_start == CONFIG_RISCV_INIT_HEAPSIZE);
+	int ret = memblock_allocator_init(&early_mmallocator, heap_start, CONFIG_RISCV_INIT_HEAPSIZE, PAGE_SIZE);
+	printf("ret : %d\n",ret);
 }
 
 void setup_vm(uintptr_t pa_dtb, uintptr_t core_id)
--- include/mm/page.h
+++ include/mm/page.h
@@ -6,7 +6,7 @@
 
 #define PAGE_OFFSET  		__ARCH_PAGE_OFFSET
 #define PAGE_SIZE     		__ARCH_PAGE_SIZE
-#define PAGE_ALIGN		((1ULL << PAGE_OFFSET) -1)
+#define PAGE_ALIGN_BIT		((1ULL << PAGE_OFFSET) -1)
 #define PAGE_UPPER_ALIGN(x)	(((x) + (PAGE_SIZE -1)) & (~PAGE_ALIGN))
 #define PAGE_DOWNS_ALIGN(x)	((x) & (~PAGE_ALIGN))
 
